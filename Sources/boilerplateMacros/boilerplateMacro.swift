cf0c82e7e1832bf04064051f38dcb40fefa0eaa7
Loki <sergiy@vynnychenko.com>
2024-01-01 22:40:17 +0200

    wip

--- Sources/boilerplateMacros/boilerplateMacro.swift
+++ Sources/boilerplateMacros/boilerplateMacro.swift
@@ -3,7 +3,18 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
-public struct NinjaModelMacro: ExtensionMacro, MemberMacro {
+public struct NinjaModelMacro: PeerMacro, MemberMacro {
+    public static func expansion(of node: SwiftSyntax.AttributeSyntax, 
+                                 providingPeersOf declaration: some SwiftSyntax.DeclSyntaxProtocol,
+                                 in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.DeclSyntax] {
+        
+        guard let classDecl = node.as(SwiftSyntax.ClassDeclSyntax.self) else {
+            fatalError("compiler: not a class")
+        }
+        
+        return ["extension \(classDecl.name): ExecutionContext, ReleasePoolOwner, ObservableObject {}"]
+    }
+    
     public static func expansion(of node: SwiftSyntax.AttributeSyntax,
                                  providingMembersOf declaration: some SwiftSyntax.DeclGroupSyntax,
                                  in context: some SwiftSyntaxMacros.MacroExpansionContext)
@@ -12,13 +23,13 @@ public struct NinjaModelMacro: ExtensionMacro, MemberMacro {
          "public let releasePool = ReleasePool()"]
     }
     
-    public static func expansion(of node: SwiftSyntax.AttributeSyntax,
-                                 attachedTo declaration: some SwiftSyntax.DeclGroupSyntax,
-                                 providingExtensionsOf type: some SwiftSyntax.TypeSyntaxProtocol,
-                                 conformingTo protocols: [SwiftSyntax.TypeSyntax],
-                                 in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
-        try [ExtensionDeclSyntax("extension \(type.trimmed): ExecutionContext, ReleasePoolOwner, ObservableObject {}")]
-    }
+//    public static func expansion(of node: SwiftSyntax.AttributeSyntax,
+//                                 attachedTo declaration: some SwiftSyntax.DeclGroupSyntax,
+//                                 providingExtensionsOf type: some SwiftSyntax.TypeSyntaxProtocol,
+//                                 conformingTo protocols: [SwiftSyntax.TypeSyntax],
+//                                 in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
+//        try [ExtensionDeclSyntax("extension \(type.trimmed): ExecutionContext, ReleasePoolOwner, ObservableObject {}")]
+//    }
     
     
 }
