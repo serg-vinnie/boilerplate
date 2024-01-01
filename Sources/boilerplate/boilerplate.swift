423308e1e7b9959494b5936997dcb9c8125a2bb9
Loki <sergiy@vynnychenko.com>
2024-01-01 16:41:11 +0200

    ninjaModel macro

--- Sources/boilerplate/boilerplate.swift
+++ Sources/boilerplate/boilerplate.swift
@@ -1,11 +1,4 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
 
-/// A macro that produces both a value and a string containing the
-/// source code that generated the value. For example,
-///
-///     #stringify(x + y)
-///
-/// produces a tuple `(x + y, "x + y")`.
-@freestanding(expression)
-public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "boilerplateMacros", type: "StringifyMacro")
+@attached(extension)
+@attached(member, names: named(context), named(releasePool))
+public macro ninjaModel() = #externalMacro(module: "MyMacroMacros", type: "NinjaModelMacro")
--- Sources/boilerplateMacros/boilerplateMacro.swift
+++ Sources/boilerplateMacros/boilerplateMacro.swift
@@ -3,31 +3,29 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
-/// Implementation of the `stringify` macro, which takes an expression
-/// of any type and produces a tuple containing the value of that expression
-/// and the source code that produced the value. For example
-///
-///     #stringify(x + y)
-///
-///  will expand to
-///
-///     (x + y, "x + y")
-public struct StringifyMacro: ExpressionMacro {
-    public static func expansion(
-        of node: some FreestandingMacroExpansionSyntax,
-        in context: some MacroExpansionContext
-    ) -> ExprSyntax {
-        guard let argument = node.argumentList.first?.expression else {
-            fatalError("compiler bug: the macro does not have any arguments")
-        }
-
-        return "(\(argument), \(literal: argument.description))"
+public struct NinjaModelMacro: ExtensionMacro, MemberMacro {
+    public static func expansion(of node: SwiftSyntax.AttributeSyntax,
+                                 providingMembersOf declaration: some SwiftSyntax.DeclGroupSyntax,
+                                 in context: some SwiftSyntaxMacros.MacroExpansionContext)
+    throws -> [SwiftSyntax.DeclSyntax] {
+        ["public let executor    = Executor.init(queue: DispatchQueue.main)",
+         "public let releasePool = ReleasePool()"]
     }
+    
+    public static func expansion(of node: SwiftSyntax.AttributeSyntax,
+                                 attachedTo declaration: some SwiftSyntax.DeclGroupSyntax,
+                                 providingExtensionsOf type: some SwiftSyntax.TypeSyntaxProtocol,
+                                 conformingTo protocols: [SwiftSyntax.TypeSyntax],
+                                 in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
+        try [ExtensionDeclSyntax("extension \(type.trimmed): ExecutionContext, ReleasePoolOwner, ObservableObject {}")]
+    }
+    
+    
 }
 
 @main
 struct boilerplatePlugin: CompilerPlugin {
     let providingMacros: [Macro.Type] = [
-        StringifyMacro.self,
+        NinjaModelMacro.self,
     ]
 }
