97bc8b47c915519f251ad7a85efb972a05eefcab
Loki <sergiy@vynnychenko.com>
2024-01-01 17:37:40 +0200

    conformances macro fix

--- Sources/boilerplate/boilerplate.swift
+++ Sources/boilerplate/boilerplate.swift
@@ -1,4 +1,8 @@
 
-@attached(extension)
+protocol ExecutionContext {}
+protocol ReleasePoolOwner {}
+protocol ObservableObject {}
+
+@attached(extension, conformances: ExecutionContext, ReleasePoolOwner, ObservableObject)
 @attached(member, names: named(executor), named(releasePool))
 public macro ninjaModel() = #externalMacro(module: "boilerplateMacros", type: "NinjaModelMacro")
